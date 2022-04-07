36d9179757357be3b1d9f002d4bec1d2cda47a5d
wuhibe <wuhibetamire710@gmail.com>
2022-04-08 00:23:28 +0300

    task complete

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+module to return pascal triangle
+'''
+
+def pascal_triangle(num):
+    lst = []
+    if num == 0:
+        return lst
+    for i in range(num):
+        lst.append([])
+        lst[i].append(1)
+        for j in range(1, i):
+            lst[i].append(lst[i - 1][j - 1] + lst[i - 1][j])
+        if i > 0:
+            lst[i].append(1)
+    return lst
