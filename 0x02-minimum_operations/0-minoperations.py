3beb7237e37d9e7a65ebf4d13bc1b40c6033e4c4
wuhibe <wuhibetamire710@gmail.com>
2022-04-21 13:02:14 +0300

    return 0 for non int values

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,7 +4,7 @@
 
 def minOperations(n):
     ''' function to get the minimum operations '''
-    if n <= 1:
+    if n <= 1 or type(n) is not int:
         return 0
     x = factorize(n)
     sum = 0
@@ -32,7 +32,7 @@ def factorize(n):
 
 
 def is_prime(n):
-    ''' function to check if a number  is prime '''
+    ''' function to check if a number is prime '''
     i = 2
     while i < n:
         if n % i == 0:
