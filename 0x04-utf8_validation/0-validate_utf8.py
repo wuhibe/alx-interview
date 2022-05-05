bb0ffd9202b94174bf1022679b7a23b0bd24cc32
wuhibe <wuhibetamire710@gmail.com>
2022-05-06 01:24:04 +0300

    0python3

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,9 +6,9 @@ def validUTF8(data):
     """ function that checks if a given data is valid utf """
     lst = [format(x, 'b') for x in data]
     for item in lst:
-        if len(item) > 8 and item[0] == 0:
+        if len(item) > 8:
             return False
-        start = int(item[0:5])
-        if start > 11110 and len(item) > 7:
+        start = int(item[:5])
+        if start > 11110:
             return False
     return True
