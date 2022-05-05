70c59104dd7469bbdd06b2caa64f32cf3a3d25f4
wuhibe <wuhibetamire710@gmail.com>
2022-05-06 01:40:19 +0300

    final

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,12 +7,8 @@ def validUTF8(data):
     lst = [format(x, 'b') for x in data]
     for item in lst:
         if len(item) > 8:
-           return False
-        elif len(item) == 8:
-            start = int(item[0:5])
-            if start > 11110:
-                return False
-        else:
-            if item[0] != '0':
-                return False
+            return False
+        start = int(item[0:5])
+        if start > 11110:
+            return False
     return True
