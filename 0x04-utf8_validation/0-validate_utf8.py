494bb830aa0981df9846e6bdf8dba6f8fc2a349a
wuhibe <wuhibetamire710@gmail.com>
2022-05-06 00:12:37 +0300

    longer than 8bit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,8 @@ def validUTF8(data):
     """ function that checks if a given data is valid utf """
     lst = [format(x, 'b') for x in data]
     for item in lst:
+        if len(item) > 8 and item[0] == 0:
+            return False
         start = int(item[0:5])
         if start > 11110:
             return False
