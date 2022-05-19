4b5b1080a5a4b73f354ae684a47fbd8576a1dc23
wuhibe <wuhibetamire710@gmail.com>
2022-05-19 22:01:22 +0300

    async/await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,19 +2,18 @@
 
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-let result = {};
-request(url, function (error, response, body) {
+
+request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const abc = JSON.parse(body).characters;
     for (let i = 0; i < abc.length; i++) {
-      request(abc[i], function (error, response, content) {
+      await request(abc[i], function (error, response, content) {
         if (error) {
           console.log(error);
         } else {
-          result = JSON.parse(content);
-          console.log(result.name);
+          console.log(JSON.parse(content).name);
         }
       });
     }
