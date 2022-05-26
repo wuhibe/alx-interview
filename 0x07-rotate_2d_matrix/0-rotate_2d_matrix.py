bca27a8473b2993be4283859506fe93baabd1b02
wuhibe <wuhibetamire710@gmail.com>
2022-05-26 19:31:34 +0300

    rotate

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def rotate_2d_matrix(matrix):
+    """ method to rotate a 2d matrix
+        counterclockwise
+    """
+    n = len(matrix)
+    m2 = [[0 for x in range(n)] for y in range(n)]
+    for i in range(n):
+        for j in range(n):
+            m2[i][n - j - 1] = matrix[j][i]
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = m2[i][j]
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,9 @@
+# 0x07. Rotate 2D Matrix
+
+## Strategy
+
+* Access the matrix columns vertically
+
+* Copy items to another temporary matrix in a back-to-front manner
+
+* Replace the items in the original matrix by the temporary matrix values
