21e60a074217224b43ec002d1c45b24246673509
wuhibe <wuhibetamire710@gmail.com>
2022-06-14 01:42:46 +0300

    promisify

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 
+const util = require('util');
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, async function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+if (process.argv[2] === undefined) {
+  console.log('Usage: node <FILENAME> <id>');
+} else {
+  (async () => {
+    const requestPromise = util.promisify(request);
+    const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+    const body = (await requestPromise(url)).body;
     const abc = JSON.parse(body).characters;
     for (let i = 0; i < abc.length; i++) {
-      await request(abc[i], function (error, response, content) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(content).name);
-        }
-      });
+      const char = (await requestPromise(abc[i])).body;
+      console.log(JSON.parse(char).name);
     }
-  }
-});
+  })();
+}
