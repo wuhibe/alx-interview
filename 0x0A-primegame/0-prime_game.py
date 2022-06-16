bd1895594c477e401fe2c05bb45b24321cab8a5e
wuhibe <wuhibetamire710@gmail.com>
2022-06-16 21:11:08 +0300

    merge functions

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
 
 
-def isPrime(n):
-    """ method to check if a number is prime """
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def game(n):
-    """ method to simulate a single game """
-    arr = [i for i in range(2, n + 1)]
-    who = True
-    for n in arr:
-        if isPrime(n):
-            who = not who
-    if who:
-        return 'Ben'
-    else:
-        return 'Maria'
-
-
 def isWinner(x, nums: list):
     """ method to find the winner of x games between maria and ben
     """
+    if x < 0 or not nums:
+        return None
     count = 1
     b = 0
     m = 0
     for n in nums:
         if count > x:
             break
-        if game(n) == 'Ben':
+        arr = [i for i in range(2, n + 1)]
+        who = True
+        for n in arr:
+            for i in range(2, int(n ** 0.5) + 1):
+                if n % i == 0:
+                    who = not who
+                    break
+            who = not who
+        if who:
             b += 1
         else:
             m += 1
         count += 1
     if b > m:
-        winner = 'Ben'
+        return 'Ben'
     elif m > b:
-        winner = 'Maria'
+        return 'Maria'
     else:
-        winner = None
-    return winner
+        return None
--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -2,6 +2,10 @@
 
 isWinner = __import__('0-prime_game').isWinner
 
-print("Winner: {}".format(isWinner(5, [4, 5, 2, 3, 1])))
-print("Winner: {}".format(isWinner(4, [4, 5, 1, 5])))
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 6, 7, 8, 9, 3])))
+print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+print("Winner: {}".format(isWinner(3, [4, 5, 1])))
+
+print("\nWinner: {}\n".format(isWinner(0, [])))
+
+print("Winner: {}".format(isWinner(3, [2, 6, 3])))
+print("Winner: {}".format(isWinner(5, [4, 13, 12, 6, 50])))
